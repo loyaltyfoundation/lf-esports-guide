1ca4ecf0b385c506dc47773c9859e400fea895f7
loyaltyfoundation <allen.rabinovich@loyaltyfoundation.org>
2024-12-04 11:06:35 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: 'loyaltyfoundation', // Usually your GitHub org/user name.
   projectName: 'lf-esports-guide', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
